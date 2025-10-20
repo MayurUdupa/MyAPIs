30043afed777f165c7b4e7c6bba1b07d22e10284
Mayur <mayur.udupa@bixbytessolutions.com>
2025-10-20 18:02:00 +0530

    5.

--- MyAPI/Controllers/AssetController.cs
+++ MyAPI/Controllers/AssetController.cs
@@ -69,7 +69,7 @@ namespace MyAPI.Controllers
 
             appDb.Assets.Remove(data);
             appDb.SaveChanges();
-            return Ok();
+            return Ok("info1");
             //mayur laptop data1
         }
     }
